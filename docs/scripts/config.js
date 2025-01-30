d5c0061c0592ceb5fbbe244b28b91a43b3266251
ghukpresales <github@ukpresales.co.uk>
2025-01-30 16:50:12 +0000

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -2,7 +2,7 @@ export default {
     // Token Implicit Grant Client ID
     clientID: 'a0e7bcd6-6d04-4a65-b7ff-e92d5da8109c',
 
-    redirectUri: 'https://ec2-18-171-169-63.eu-west-2.compute.amazonaws.com/',
+    redirectUri: 'https://ec2-35-178-186-246.eu-west-2.compute.amazonaws.com/',
 
     genesysCloud: {
         // Genesys Cloud region
@@ -10,5 +10,5 @@ export default {
         region: 'euw2.pure.cloud'
     },
 
-    translateServiceURI: 'https://ec2-18-171-169-63.eu-west-2.compute.amazonaws.com/translate'
+    translateServiceURI: 'ec2-35-178-186-246.eu-west-2.compute.amazonaws.com/translate'
 } 
