a5a5cc2ea70a959d9a55b345bc0e1182c1b39ba5
ghukpresales <github@ukpresales.co.uk>
2025-01-30 11:35:57 +0000

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -2,13 +2,13 @@ export default {
     // Token Implicit Grant Client ID
     clientID: 'a0e7bcd6-6d04-4a65-b7ff-e92d5da8109c',
 
-    redirectUri: 'https://awstranslatemessaging-7vzhyfekx-vercel-ukpresales-projects.vercel.app/',
+    redirectUri: 'https://ec2-18-171-169-63.eu-west-2.compute.amazonaws.com/',
 
     genesysCloud: {
         // Genesys Cloud region
         // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
-        region: 'mypurecloud.com'
+        region: 'euw2.pure.cloud'
     },
 
-    translateServiceURI: 'https://awstranslatemessaging-7vzhyfekx-vercel-ukpresales-projects.vercel.app/translate'
+    translateServiceURI: 'https://ec2-18-171-169-63.eu-west-2.compute.amazonaws.com/translate'
 } 
